1dc36777ff3e854cc0f50ae5591757e6827e4cef
javalenja <javalenja@gmail.com>
2019-07-07 21:11:14 -0500

    Modificaciones para añadir H2.

---
+++ src/main/java/org/h2/server/web/DbStarter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.h2.server.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author 8028609
+ */
+public class DbStarter implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
